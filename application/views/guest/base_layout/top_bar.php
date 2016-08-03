fb82c6d7697730400a76335a741604ccfc968ac1
MinhNV <minhnv@ows.vn>
2016-08-03 18:07:34 +0700

    add image table for homepage

--- application/views/guest/base_layout/top_bar.php
+++ application/views/guest/base_layout/top_bar.php
@@ -9,11 +9,11 @@
             <i class="material-icons" style="color: black; right: 0;">search</i>
         </div>
         <ul class="right hide-on-med-and-down">
-            <li><a href="#edu_method" id="edu-method"><?php echo $menu_1; ?></a></li>
-            <li><a href="#edu_program" id="edu-program"><?php echo $menu_2; ?></a></li>
-            <li><a href="#edu_library" id="edu-library"><?php echo $menu_3; ?></a></li>
-            <li><a href="#answer_question" id="answer-question"><?php echo $menu_4; ?></a></li>
-            <li><a href="#intro_beedu" id="intro-beedu"><?php echo $menu_5; ?></a></li>
+            <li><a href="home#edu_method" id="edu-method"><?php echo $menu_1; ?></a></li>
+            <li><a href="home#edu_program" id="edu-program"><?php echo $menu_2; ?></a></li>
+            <li><a href="home#edu_library" id="edu-library"><?php echo $menu_3; ?></a></li>
+            <li><a href="home#answer_question" id="answer-question"><?php echo $menu_4; ?></a></li>
+            <li><a href="home#intro_beedu" id="intro-beedu"><?php echo $menu_5; ?></a></li>
         </ul>
         <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
