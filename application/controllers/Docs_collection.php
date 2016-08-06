340b280b69dfc80693fb5ca1dc75d53a3aa8bd97
MinhNV <mjnhm3o94@gmail.com>
2016-08-06 17:20:18 +0700

    load document by category

--- application/controllers/Docs_collection.php
+++ application/controllers/Docs_collection.php
@@ -5,16 +5,31 @@
  * User: hieuapp
  * Date: 04/08/2016
  * Time: 19:31
+ * @property M_documents  m_documents
+ * @property M_categories m_categories
  */
-class Docs_collection extends Guest_layout
-{
-    function __construct(){
+class Docs_collection extends Guest_layout {
+    function __construct() {
         parent::__construct();
-        $this->load->model('M_system_config', 'm_system_config');
+        $this->load->model('M_documents', 'm_documents');
+        $this->load->model('M_categories', 'm_categories');
     }
 
-    public function index(){
-        $content = $this->load->view("guest/home/docs_collection",null, TRUE);
+    public function index() {
+        $list_category = $this->m_categories->get_all();
+        $document_by_category = [];
+        foreach ($list_category as $category) {
+            $where = [
+                'ct.id' => $category->id,
+            ];
+            $list_document_in_category = $this->m_documents->get_list_filter($where, [], []);
+            array_push($document_by_category, [
+                'category_name'             => $category->name,
+                'list_document_in_category' => $list_document_in_category,
+            ]);
+        }
+        $data["document_by_category"] = $document_by_category;
+        $content = $this->load->view("guest/home/docs_collection", $data, TRUE);
         $this->show_page($content);
     }
 }
--- application/views/guest/home/docs_collection.php
+++ application/views/guest/home/docs_collection.php
@@ -1,121 +1,32 @@
+<?php foreach ($document_by_category as $document_category_item) {
+    ?>
+    <div class="container">
+        <div class="row column text-center">
+            <a class="group-title left" href="<?php echo base_url("document"); ?>">
+                <i class="material-icons vertical-sub">storage</i><?php echo $document_category_item['category_name']; ?>
+            </a>
+        </div>
+        <hr>
+        <div class="row">
+            <?php
+            if (count($document_category_item['list_document_in_category'])) {
+                foreach ($document_category_item['list_document_in_category'] as $document) {
+                    ?>
+                    <div class="col s6 m6 l2">
+                        <a href="<?php echo base_url("document_preview"); ?>">
+                            <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png"); ?>">
+                        </a>
+                        <a href="<?php echo base_url("document_preview"); ?>"><?php echo $document->name; ?></a></h5>
+                        <br><span>400 downloads</span>
+                        <button class="waves-effect waves-light btn">Download</button>
+                    </div>
 
-<div class="container">
-    <div class="row column text-center">
-        <a class="group-title left" href="<?php echo base_url("document");?>">
-            <i class="material-icons vertical-sub">storage</i> Đề thi đại học</a>
+                    <?php
+                }
+            } else { ?>
+                Chưa có tài liệu nào trong chuyên mục này!!!
+            <?php } ?>
+        </div>
     </div>
-    <hr>
-    <div class="row">
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi đại học môn Toán khối A 2016</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi đại học môn Toán khối A 2016</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi đại học môn Toán khối A 2016</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi đại học môn Toán khối A 2016</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi đại học môn Toán khối A 2016</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi đại học môn Toán khối A 2016</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <a class="right" href="<?php echo base_url("document");?>"><br>Xem thêm</a>
-    </div>
-
-</div>
-
-<div class="container">
-    <div class="row column text-center">
-        <a class="group-title left" href="<?php echo base_url("document");?>">
-            <i class="material-icons vertical-sub">storage</i> Đề thi học sinh giỏi</a>
-    </div>
-    <hr>
-    <div class="row">
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi học sinh giỏi lớp 10</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi học sinh giỏi lớp 10</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi học sinh giỏi lớp 10</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi đại học môn Toán khối A 2016</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi đại học môn Toán khối A 2016</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <div class="col s6 m6 l2">
-            <a href="<?php echo base_url("document_preview");?>">
-                <img class="thumbnail" src="<?php echo base_url("assets/images/300x400.png");?>">
-            </a>
-            <a href="<?php echo base_url("document_preview");?>">Đề thi đại học môn Toán khối A 2016</a></h5>
-            <br><span>400 downloads</span>
-            <button class="waves-effect waves-light btn">Download</button>
-        </div>
-        <br>
-        <a class="right" href="<?php echo base_url("document");?>"><br>Xem thêm </a>
-    </div>
-
-</div>
+    <?php
+} ?>
