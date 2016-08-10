bb1e0de4c313cb7af6c464b49fe440983de80764
MinhNV <mjnhm3o94@gmail.com>
2016-08-10 16:26:16 +0700

    Edit view of document/view_category

--- application/views/guest/home/document-preview.php
+++ application/views/guest/home/document-preview.php
@@ -9,7 +9,7 @@
             <div class="col s12 m4 l8">
                 <iframe
                     src="http://docs.google.com/gview?url=http://infolab.stanford.edu/pub/papers/google.pdf&embedded=true"
-                    style="width:700px; height:500px;" frameborder="0"></iframe>
+                    style="width:100%; height:500px;" frameborder="0"></iframe>
             </div>
             <div class="col s12 m4 l4">
                 <div class="collection">
