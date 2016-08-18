699880c5268c5c79c9445668e3000653c908ead1
hieuapp <trunghieu.bka57@gmail.com>
2016-08-18 11:44:33 +0700

    sua css phuong phap hoc

--- application/views/guest/base_layout/assets_footer.php
+++ application/views/guest/base_layout/assets_footer.php
@@ -4,4 +4,21 @@
 <script src="<?php echo base_url("assets/js/material_js/init.js"); ?>"></script>
 <script src="<?php echo base_url("assets/js/jquery-3.1.0.js"); ?>"></script>
 <script src="<?php echo base_url("assets/js/index.js"); ?>"></script>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '1798369220409564',
+            xfbml      : true,
+            version    : 'v2.7'
+        });
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 
