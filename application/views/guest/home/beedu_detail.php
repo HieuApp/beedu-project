3b347bc3501ce08d9fe36deef5180ebb201bd9f5
MinhNV <mjnhm3o94@gmail.com>
2016-08-25 22:11:13 +0700

    add new db to view intro

--- application/views/guest/home/beedu_detail.php
+++ application/views/guest/home/beedu_detail.php
@@ -3,17 +3,22 @@
         <div class="col s12 m12 l8 offset-l2 card letter-padding">
             <h5 class="black-text center">Thư ngỏ</h5>
             <p class="letter-text">
-                <?php echo $hello_content; ?>
+                <?php echo $title; ?>
             </p>
-            <div><img src="<?php echo base_url($image); ?>" alt="Unsplashed background img 3">
+            <p class="letter-text">
+                <?php echo $par1; ?>
+
+            </p>
+            <div class="center"><img src="<?php echo base_url($image); ?>" alt="Unsplashed background img 3">
             </div>
             <p class="letter-text">
-                <?php echo $para8; ?>
+                <?php echo $par2; ?>
 
             </p>
             <p class="author-sign">Đức Anh</p>
             <div class="center">
-                <a class="waves-effect waves-light btn blue" href="<?php echo base_url("home#edu_method"); ?>">Xem phương pháp học</a>
+                <a class="waves-effect waves-light btn blue" href="<?php echo base_url("home#edu_method"); ?>">Xem
+                    phương pháp học</a>
             </div>
 
         </div>
